6290a2cacc6f1fab9a95c92b821c6fc1b88da173
Plover Brown <rellwoos@gmail.com>
2020-09-21 13:49:45 -0700

    added functionality: delete and add roles and departments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Role = require("./lib/classes/role");
 const Department = require("./lib/classes/department");
 const inquirer = require("inquirer");
 const queries = require("./lib/functions/queryfunctions");
+const { addToDB } = require("./lib/functions/queryfunctions");
 
 
 
@@ -19,22 +20,22 @@ const questions = [
         type: "list",
         message: `Please choose an employee to update: `,
         name: "choice",
-        choices: queries.employeeList,
+        choices: queries.employeeChoiceList,
         when: (answers) => ((answers.action === "Update an employee's role") || (answers.action === "Update an employee's manager"))
     },
     {
         type: "list",
-        message: `Please choose an new role: `,
+        message: `Please choose a new role: `,
         name: "chosenRole",
         choices: queries.roleChoiceList,
         when: (answers) => answers.action === "Update an employee's role",
     },
     {
         type: "list",
-        message: `Please choose an employee to remove: `,
-        name: "choice",
-        choices: queries.employeeList,
-        when: (answers) => (answers.action === "Remove an employee")
+        message: `Please choose a new manager: `,
+        name: "chosenManager",
+        choices: queries.managerChoiceList,
+        when: (answers) => answers.action === "Update an employee's manager",
     },
     {
         type: "input",
@@ -76,6 +77,58 @@ const questions = [
         choices: queries.managerChoiceList,
         when: (answers) => answers.action === "View employees by manager"
     },
+    {
+        type: "input",
+        message: "Role title: ",
+        name: "roleTitle",
+        when: (answers) => answers.action === "Add a role"
+    },
+    {
+        type: "input",
+        message: "Role salary: ",
+        name: "roleSalary",
+        when: (answers) => answers.action === "Add a role"
+    },
+    {
+        type: "list",
+        message: "Select role department: ",
+        name: "roleDepartment",
+        choices: queries.departmentChoiceList,
+        when: (answers) => answers.action === "Add a role"
+    },
+    {
+        type: "input",
+        message: "Department name: ",
+        name: "departmentName",
+        when: (answers) => answers.action === "Add a department"
+    },
+    {
+        type: "number",
+        message: "Department budget: ",
+        name: "departmentBudget",
+        when: (answers) => answers.action === "Add a department"
+    },
+    {
+        type: "list",
+        message: "Select role to remove: ",
+        name: "removeRole",
+        choices: queries.roleChoiceList,
+        when: (answers) => answers.action === "Remove a role"
+    },
+    {
+        type: "list",
+        message: "Select department to remove: ",
+        name: "removeDepartment",
+        choices: queries.departmentChoiceList,
+        when: (answers) => answers.action === "Remove a department"
+    },
+    {
+        type: "list",
+        message: "Select employee to remove: ",
+        name: "removeEmployee",
+        choices: queries.employeeChoiceList,
+        when: (answers) => answers.action === "Remove an employee"
+    },
 
 ]
 
@@ -100,15 +153,29 @@ connections.connection.connect(function(err) {
         } else if (answers.action === "View all employees"){
             queries.readAll();
         } else if (answers.action === "View employees by department"){
-            queries.readDepartments(answers.department)
+            queries.readByDepartment(answers.department)
         } else if (answers.action === "View employees by manager"){
-            queries.readManagers(answers.viewManager)
+            queries.readByManager(answers.viewManager)
         } else if (answers.action === "Update an employee's role"){
             queries.updateEmployeeRole(answers.choice, answers.chosenRole);
+        } else if (answers.action === "Update an employee's manager"){
+            queries.updateEmployeeManager(answers.choice, answers.chosenManager);
         } else if (answers.action === "View all roles"){
             queries.viewAllRoles();
         } else if (answers.action === "View all departments"){
             queries.viewAllDepartments();
+        } else if (answers.action === "Add a role"){
+            const newRole = new Role(answers.roleTitle, answers.roleSalary, queries.departmentList[answers.roleDepartment]);
+            addToDB("roles", newRole);
+        } else if (answers.action === "Add a department"){
+            const newDepartment = new Department(answers.departmentName, answers.departmentBudget);
+            addToDB("departments", newDepartment);
+        } else if (answers.action === "Remove a role"){
+            queries.deleteFromDB("roles", "id", queries.roleList[answers.removeRole].id, answers.removeRole);
+        } else if (answers.action === "Remove a department"){
+            queries.deleteFromDB("departments", "id", queries.departmentList[answers.removeDepartment], answers.removeDepartment);
+        } else if (answers.action === "Remove an employee"){
+            console.log(answers.removeEmployee);
         }
 
 
