5931e92d2bbac95b6cd790a0af0ac9e19cd9db1d
Plover Brown <rellwoos@gmail.com>
2020-09-23 15:27:48 -0700

    added function: delete employee

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const Role = require("./lib/classes/role");
 const Department = require("./lib/classes/department");
 const inquirer = require("inquirer");
 const queries = require("./lib/functions/queryfunctions");
-const { addToDB } = require("./lib/functions/queryfunctions");
 
 
 
 //inquirer questions
-const questions = [
+let questions = [
     {
         type: "list",
         message: "Please Choose an action: ",
         name: "action",
-        choices: ["View all employees", "View employees by department", "View employees by manager", "Add an employee", "Update an employee's role", "Update an employee's manager", "Remove an employee", "View all roles", "Add a role", "Remove a role", "View all departments", "Add a department", "Remove a department"]
+        choices: ["View all employees", "View employees by department", "View employees by manager", "Add an employee", "Update an employee's role", "Update an employee's manager", "Remove an employee", "View all roles", "Add a role", "Remove a role", "View all departments", "Add a department", "Remove a department", "EXIT"]
     },
     {
         type: "list",
@@ -129,56 +128,63 @@ const questions = [
         choices: queries.employeeChoiceList,
         when: (answers) => answers.action === "Remove an employee"
     },
-
+    
 ]
 
-//action upon DB connections.connection
-connections.connection.connect(function(err) {
-    if (err) throw err;
 
-    //initial setup of employee varibles
-    queries.setVariables();
+    //action upon DB connections.connection
+    connections.connection.connect(function(err) {
+        if (err) throw err;
 
-    //prompt for user input
-    inquirer.prompt(questions).then(function(answers){
-        if (answers.action === "Add an employee") {
-            //change out NONE for the mySQL parseable NULL
-            if (answers.manager === "NONE"){
-                answers.manager = "NULL"
-            }
-            //create an object with the employee information to feed the DB
-            const newEmployee = new Employee(answers.firstName, answers.lastName, queries.roleList[answers.role].id, queries.managerList[answers.manager]);
-            queries.addToDB("employees", newEmployee);
+        
+function runEmployeeTracker(){
+        //initial setup of employee varibles
+        queries.setVariables().then(
+            
+                //prompt for user input
+                inquirer.prompt(questions).then(function(answers){
+                    if (answers.action === "Add an employee") {
+                        let newEmployee;
 
-        } else if (answers.action === "View all employees"){
-            queries.readAll();
-        } else if (answers.action === "View employees by department"){
-            queries.readByDepartment(answers.department)
-        } else if (answers.action === "View employees by manager"){
-            queries.readByManager(answers.viewManager)
-        } else if (answers.action === "Update an employee's role"){
-            queries.updateEmployee("role", answers.choice, answers.chosenRole);
-        } else if (answers.action === "Update an employee's manager"){
-            queries.updateEmployee("manager", answers.choice, answers.chosenManager);
-        } else if (answers.action === "View all roles"){
-            queries.viewAllRoles();
-        } else if (answers.action === "View all departments"){
-            queries.viewAllDepartments();
-        } else if (answers.action === "Add a role"){
-            const newRole = new Role(answers.roleTitle, answers.roleSalary, queries.departmentList[answers.roleDepartment]);
-            addToDB("roles", newRole);
-        } else if (answers.action === "Add a department"){
-            const newDepartment = new Department(answers.departmentName, answers.departmentBudget);
-            addToDB("departments", newDepartment);
-        } else if (answers.action === "Remove a role"){
-            queries.deleteFromDB("roles", "id", queries.roleList[answers.removeRole].id, answers.removeRole);
-        } else if (answers.action === "Remove a department"){
-            queries.deleteFromDB("departments", "id", queries.departmentList[answers.removeDepartment], answers.removeDepartment);
-        } else if (answers.action === "Remove an employee"){
-            console.log(answers.removeEmployee);
-        }
-
-
-        connections.connection.end()
-    })  
-})
+                        //create an object with the employee information to feed the DB
+                        if (answers.manager === "NONE"){
+                            newEmployee = new Employee(answers.firstName, answers.lastName, queries.roleList[answers.role].id);
+                        } else {
+                            newEmployee = new Employee(answers.firstName, answers.lastName, queries.roleList[answers.role].id, queries.managerList[answers.manager]);
+                        }
+                        queries.addToDB("employees", newEmployee).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "View all employees"){
+                        queries.readAll().then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "View employees by department"){
+                        queries.readByDepartment(answers.department).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "View employees by manager"){
+                        queries.readByManager(answers.viewManager).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "Update an employee's role"){
+                        queries.updateEmployee("role", answers.choice, answers.chosenRole).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "Update an employee's manager"){
+                        queries.updateEmployee("manager", answers.choice, answers.chosenManager).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "View all roles"){
+                        queries.viewAllRoles().then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "View all departments"){
+                        queries.viewAllDepartments().then(runEmployeeTracker);
+                    } else if (answers.action === "Add a role"){
+                        const newRole = new Role(answers.roleTitle, answers.roleSalary, queries.departmentList[answers.roleDepartment]);
+                        addToDB("roles", newRole).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "Add a department"){
+                        const newDepartment = new Department(answers.departmentName, answers.departmentBudget);
+                        addToDB("departments", newDepartment).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "Remove a role"){
+                        queries.deleteFromDB("roles", "id", queries.roleList[answers.removeRole].id, answers.removeRole).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "Remove a department"){
+                        queries.deleteFromDB("departments", "id", queries.departmentList[answers.removeDepartment], answers.removeDepartment).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "Remove an employee"){
+                        queries.deleteFromDB("employees", "id", queries.employeeList[answers.removeEmployee], answers.removeEmployee).then(setTimeout(runEmployeeTracker, 500));
+                    } else if (answers.action === "EXIT"){
+                        connections.connection.end();
+                    }
+                })//end inquirer prompt
+        )//end then 
+    }
+    
+runEmployeeTracker();
+});
